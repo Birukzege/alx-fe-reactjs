1d01469a03655d3e7dd03f84037c7ea1b2f51bcf
Biruk Zegene <brukzegetaye@gmail.com>
2024-08-11 14:38:22 +0300

    modifications

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+
+import WelcomeMessage from './WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,6 +10,10 @@ function App() {
 
   return (
     <>
+        <div>
+            {/* <h1>Welcome to My React App</h1> */}
+            <WelcomeMessage />
+        </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,6 +36,7 @@ function App() {
       </p>
     </>
   )
+  
 }
 
 export default App
