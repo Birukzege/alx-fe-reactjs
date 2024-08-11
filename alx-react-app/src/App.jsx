12408b6daed53cb884088269f1bc94a6771f216b
Biruk Zegene <brukzegetaye@gmail.com>
2024-08-11 15:00:47 +0300

    modified

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,13 +5,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <div>
+     <div>
+            <Header />
+            <MainContent />
+            <Footer />
+    </div>
+     <div>
         <h1>Hello everyone, I am learning React at ALX!</h1>
         <p>I am learning about JSX!</p>
             <WelcomeMessage />
