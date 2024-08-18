502f7f51e7fa8aff137aa03b7ae03101bb17848a
Biruk Zegene <brukzegetaye@gmail.com>
2024-08-18 11:23:08 +0300

    added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -9,6 +9,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
 
 
 function App() {
@@ -16,6 +18,11 @@ function App() {
 
   return (
     <>
+
+       <div>
+            <h1>Counter Application</h1>
+            <Counter />
+        </div>
     <div>
             <h1>User Profile</h1>
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
