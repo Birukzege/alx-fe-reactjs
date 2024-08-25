4ac8866686df00522e534c1f3d51702b04f62ec3
Biruk Zegene <brukzegetaye@gmail.com>
2024-08-25 15:30:29 +0300

    addded

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
 // ... other imports
+import AddRecipeForm from './components/AddRecipeForm'; // Import AddRecipeForm component
 
 function App() {
   return (
