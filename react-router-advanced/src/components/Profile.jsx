9e5434668d0819d6bd194e3f763a090a9cc820cf
Biruk Zegene <brukzegetaye@gmail.com>
2024-09-01 13:51:40 +0300

    F

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,22 +1,22 @@
-   // src/components/Profile.jsx
+// src/components/Profile.jsx
 
-   import React from 'react';
-   import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
-   const Profile = () => {
-       return (
-           <div>
-               <h1>User Profile</h1>
-               <nav>
-                   <Link to="details">Profile Details</Link>
-                   <Link to="settings">Profile Settings</Link>
-               </nav>
-               <Outlet />
-           </div>
-       );
-   };
+const Profile = () => {
+    return (
+        <div>
+            <h1>User Profile</h1>
+            <nav>
+                <Link to="details">Profile Details</Link>
+                <Link to="settings">Profile Settings</Link>
+            </nav>
+            {/* This Outlet renders the nested routes */}
+            <Outlet />
+        </div>
+    );
+};
 
-   export default Profile;
-
-   
-
+export default Profile;
