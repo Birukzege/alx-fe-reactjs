b6238c90f720f612db04fa4cd6fd9223c9623915
Biruk Zegene <brukzegetaye@gmail.com>
2024-09-01 13:08:23 +0300

    add

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import RegistrationForm from './components/RegistrationForm';
+import RegistrationForm from '../components/formikForm';
 
 const App = () => {
     const validationSchema = Yup.object().shape({
