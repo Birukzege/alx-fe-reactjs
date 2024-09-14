41db7b41b94237d06f7e5efda3b1dd52de32b818
Biruk Zegene <brukzegetaye@gmail.com>
2024-09-15 00:58:04 +0300

    added

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // Or 'media' or 'class'
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
